f1820e2e9c50125d4ba261ed96caff3d10211662
Elliot Hesp <elliot.hesp@gmail.com>
2025-01-14 19:43:55 +0000

    test: Initial testing setup (#64)

--- src/cors.ts
+++ src/cors.ts
@@ -4,7 +4,7 @@ export const corsHeaders = {
     'Access-Control-Allow-Headers':
         'Authorization, Content-Type, X-Starbase-Source, X-Data-Source',
     'Access-Control-Max-Age': '86400',
-}
+} as const
 
 export function corsPreflight(): Response {
     return new Response(null, {
