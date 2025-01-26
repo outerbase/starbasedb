5bd6981c9938287cb61d51e75a5e54846545c504
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2025-01-25 19:29:34 -0500

    Vitest config updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,15 @@ export default defineConfig({
     test: {
         coverage: {
             provider: 'istanbul',
-            reporter: ['text', 'html', 'json', 'lcov'],
+            reporter: ['text', 'html', 'json', 'json-summary', 'lcov'],
             include: ['src/**/*.ts'],
             exclude: ['**/node_modules/**'],
+            thresholds: {
+                lines: 60,
+                functions: 60,
+                branches: 60,
+                statements: 60,
+            },
         },
     },
 })
