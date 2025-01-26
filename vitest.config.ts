34f779c40fc9cd61ee9fd6cd8c5496b1c1437290
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2025-01-26 12:40:07 -0500

    Update yaml

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
     test: {
         coverage: {
             provider: 'istanbul',
-            reporter: ['text', 'html', 'json', 'lcov'],
+            reporter: ['text', 'html', 'json', 'lcov', 'json-summary'], // Ensure json-summary is included
             include: ['src/**/*.ts'],
             exclude: ['**/node_modules/**'],
-            reportOnFailure: true,
+            reportOnFailure: true, // Ensures the report is generated even if tests fail
             thresholds: {
                 lines: 60,
                 branches: 60,
