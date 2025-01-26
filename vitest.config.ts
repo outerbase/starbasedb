a1fbab766ccc7d86d7c0d3461faa60635420591a
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2025-01-25 19:22:32 -0500

    Include config

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['text', 'html', 'json', 'lcov'],
+            include: ['src/**/*.ts'],
+            exclude: ['**/node_modules/**'],
+            thresholds: {
+                lines: 60,
+                branches: 60,
+                functions: 60,
+                statements: 60,
+            },
+        },
+    },
+})
