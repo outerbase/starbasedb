d1faba87cbc4de6f4441e9cdcfb6590b06664653
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2025-02-15 16:43:54 -0500

    Provide a template page

--- plugins/interface/components/theme/ThemeSelector.tsx
+++ plugins/interface/components/theme/ThemeSelector.tsx
@@ -3,64 +3,45 @@ import { cn } from '../../utils/index'
 // import { Moon, Sun } from '@phosphor-icons/react'
 import { useState } from 'hono/jsx'
 
-// type ThemeSelectorProps = {
-//   onClick: () => void
-//   theme: 'light' | 'dark'
-// }
-
-const ThemeSelector = () =>
-    // { onClick, theme }: ThemeSelectorProps
-    {
-        const [theme, setTheme] = useState<'dark' | 'light'>(() => {
-            if (typeof window !== 'undefined') {
-                const stored = document.cookie
-                    .split('; ')
-                    .find((row) => row.startsWith('theme='))
-                    ?.split('=')[1] as 'light' | 'dark'
-                return stored || 'light'
-            }
-            return 'light'
-        })
-
-        useTheme(theme)
-
-        const toggleTheme = () => {
-            setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'))
+const ThemeSelector = () => {
+    const [theme, setTheme] = useState<'dark' | 'light'>(() => {
+        if (typeof window !== 'undefined') {
+            const stored = document.cookie
+                .split('; ')
+                .find((row) => row.startsWith('theme='))
+                ?.split('=')[1] as 'light' | 'dark'
+            return stored || 'light'
         }
+        return 'light'
+    })
 
-        return (
-            <button
-                className="flex size-8 cursor-pointer items-center justify-center rounded-md hover:bg-neutral-200/60 dark:hover:bg-neutral-900"
-                onClick={() => toggleTheme()}
-            >
-                <div
-                    className={cn('hidden', {
-                        'animate-fade block': theme === 'dark',
-                    })}
-                >
-                    Moon
-                </div>
-                <div
-                    className={cn('animate-fade block', {
-                        hidden: theme === 'dark',
-                    })}
-                >
-                    Sun
-                </div>
-                {/* <Moon
-          weight="bold"
-          className={cn('hidden', {
-            'animate-fade block': theme === 'dark',
-          })}
-        />
-        <Sun
-          weight="bold"
-          className={cn('animate-fade block', {
-            hidden: theme === 'dark',
-          })}
-        /> */}
-            </button>
-        )
+    useTheme(theme)
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'))
     }
+
+    return (
+        <button
+            className="flex cursor-pointer items-center justify-center rounded-md hover:bg-neutral-200/60 dark:hover:bg-neutral-900"
+            onClick={() => toggleTheme()}
+        >
+            <div
+                className={cn('hidden', {
+                    'animate-fade block': theme === 'dark',
+                })}
+            >
+                Dark
+            </div>
+            <div
+                className={cn('animate-fade block', {
+                    hidden: theme === 'dark',
+                })}
+            >
+                Light
+            </div>
+        </button>
+    )
+}
 
 export default ThemeSelector
---
+++ plugins/interface/pages/template/index.tsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'hono/jsx'
+import { hydrateRoot } from 'hono/jsx/dom/client'
+
+import { Button } from '@interface/components/button/Button'
+import { Label } from '@interface/components/label/Label'
+import { Input } from '@interface/components/input/Input'
+import { Avatar } from '@interface/components/avatar'
+import { Card } from '@interface/components/card'
+import { Select } from '@interface/components/select'
+import { Toggle } from '@interface/components/toggle'
+import ThemeSelector from '@interface/components/theme/ThemeSelector'
+
+const root = document.querySelector(
+    '#root[data-client="template"]'
+) as HTMLElement
+
+type ServerProps = {}
+
+// Get server props from the data attribute
+const serverProps = root
+    ? (JSON.parse(root.dataset.serverProps || '{}') as ServerProps)
+    : ({} as ServerProps)
+
+if (root) {
+    hydrateRoot(
+        root,
+        <StrictMode>
+            <Template {...serverProps} />
+        </StrictMode>
+    )
+}
+
+function Template({}: ServerProps) {
+    return (
+        <section>
+            <h1>Template Page</h1>
+        </section>
+    )
+}
